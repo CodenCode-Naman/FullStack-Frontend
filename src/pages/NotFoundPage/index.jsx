68dbadda1922b58d1decf0a1bd6eea86f1738d22
CodenCode-Naman <nbhatia_be19@thapar.edu>
2023-03-10 22:08:26 +0530

    feat: add Not Found Page

---
+++ src/pages/NotFoundPage/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './NotFoundPage.css';
+
+export default function NotFoundPage() {
+    return (
+        <div className="notFoundContents">
+            <p className="notFoundMsg">Oops!, page not found!</p>
+        </div>
+    );
+}
