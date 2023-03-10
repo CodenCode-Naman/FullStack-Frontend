d1bd4101f243922feaf526d25b45e5467e9b5890
CodenCode-Naman <nbhatia_be19@thapar.edu>
2023-03-10 17:35:54 +0530

    chore: add routes to pages

---
+++ src/constants/routes.js
@@ -0,0 +1,6 @@
+export const ERROR_ROUTE = '/error';
+export const NOT_FOUND_ROUTE = '*';
+
+export const CONTENT_ROUTE = '/contentType/:contentId';
+export const CONTENT_TYPE_ROUTE = '/contentType';
+export const LANDING_ROUTE = '/';
