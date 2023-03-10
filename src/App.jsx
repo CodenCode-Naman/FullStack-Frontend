b0fd1bff4350b9cbd7d66a39c5f94163eaa37daa
CodenCode-Naman <nbhatia_be19@thapar.edu>
2023-03-10 23:37:49 +0530

    feat: complete App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginUtils from './utils/loginUtils';
+import { ErrorPage, NotFoundPage, LandingPage, LoginPage } from './pages';
+import { ERROR_ROUTE, CONTENT_TYPE_ROUTE, LANDING_ROUTE } from './constants/routes';
 
 function App() {
   return (
     <div className="App">
+        <BrowserRouter>
+            <Routes>
+                <Route path={LANDING_ROUTE} element={<LoginPage />} />
+                <Route path={LANDING_ROUTE} element={<LoginUtils />} />
+                <Route path={CONTENT_TYPE_ROUTE} element={<LandingPage />} />
+            </Routes>
+            <Route path={`${ERROR_ROUTE}/:errorCode?`} element={<ErrorPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+        </BrowserRouter>
     </div>
   );
 }
