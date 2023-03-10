fe5c5c7c47525276bbb41b93743ad38c679ef84d
CodenCode-Naman <nbhatia_be19@thapar.edu>
2023-03-10 22:12:11 +0530

    feat: add Login Page

---
+++ src/pages/LoginPage/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Login } from "../../components/Login";
+
+import "./LoginPage.css";
+
+export default function LoginPage() {
+    return (
+        <div>
+            <Login />
+        </div>
+    );
+}
